cbf430b1295d86aa731207c85f0bc10c0a425d48
Mark Pieszak <hello@trilon.io>
2019-07-13 12:01:55 -0400

    update public api exports

--- libraries/ng-universal/src/public-api.ts
+++ libraries/ng-universal/src/public-api.ts
@@ -9,6 +9,8 @@ export * from './lib/directives/is-browser.directive';
 export * from './lib/directives/is-server.directive';
 export * from './lib/services/platform.service';
 export * from './lib/services/window.service';
+export * from './lib/services/seo.service';
+export * from './lib/services/link.service';
 
 export * from './lib/util/bootstrap.helpers';
 export { createWindowMocks } from './lib/util/create-window-mocks';
